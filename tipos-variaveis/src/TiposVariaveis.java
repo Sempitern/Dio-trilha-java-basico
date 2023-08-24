b1172bf8200169abd3509d46ca3a64fcf7ce6198
Sempitern <marcoswilian187@gmail.com>
2023-08-24 20:05:00 -0300

    Final da aula de tipos e variaveis.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String myName = "Marcos";
+        double minimumWage = 2500;
+        short smallNumber = 1;
+        int number = smallNumber;
+        short smallNumber2 = (short)number; /*casting */
+    
+        int num = 5;
+
+        num = 10;
+        System.out.println(num);
+
+        final double PI = 3.14;
+
+
     }
 }
