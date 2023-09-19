b2b676747aa06ac03e3620c7a681513bfa59a486
Sempitern <marcoswilian187@gmail.com>
2023-09-19 02:03:42 -0300

    Definindo uma nova string

--- loops/Loops/src/ExemploForArrays.java
+++ loops/Loops/src/ExemploForArrays.java
@@ -2,7 +2,8 @@ public class ExemploForArrays {
     public static void main(String[] args) throws Exception {
         String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
 
-        for (int x=0; x<alunos.length; x++)
-        System.out.println("O aluno no indice x= "+ x + " é " + alunos[x]);
+        for (String aluno : alunos){
+        System.out.println("Nome do aluno é: " + aluno);
     }
+}
 }
